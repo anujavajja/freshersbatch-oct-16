650d5145aeb23b6f270ba0ca3295050efdcd61d0
anujavajja <115220049+anujavajja@users.noreply.github.com>
2022-11-08 10:16:17 +0530

    Add files via upload

---
+++ Array.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+    internal class Array
+    {
+        public static void Main()
+        {
+            ArrayList array = new ArrayList();
+           
+        }
+    }
+}
